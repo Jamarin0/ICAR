42f2071473b19ae22f5c0eb47f4c4c47db60dcc4
Jamarin0 <jgjsguilherme@gmail.com>
2021-09-06 13:18:07 -0300

    fix: Upload avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+import { request } from "express";
 import multer from "multer";
 import { resolve } from "path";
 
@@ -6,7 +8,13 @@ export default {
     upload(folder: string) {
         return {
             storage: multer.diskStorage({
-                
+                destination: resolve(__dirname, "..", "..", folder),
+                filename: (request, file, callback ) => {
+                    const fileHash = crypto.randomBytes(16).toString("hex");
+                    const fileName = `${fileHash}-${file.originalname}`;
+
+                    return callback(null, fileName);
+                }
             })
         }
     }
--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file";
+
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -8,17 +11,19 @@ interface IRequest {
 
 @injectable()
 class UpdateUserAvatarUseCase {
-
     constructor(
         @inject("UsersRepository")
         private UsersRepository: IUsersRepository
-    ) {}
+    ) { }
 
-    async execute({ user_id, avatar_file}: IRequest): Promise<void> {
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.UsersRepository.findById(user_id);
 
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
         user.avatar = avatar_file
-        
+
         await this.UsersRepository.create(user);
 
     }
