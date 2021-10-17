055443b9cefad0b748a06c904bb1705f2f9919b0
Jamarin0 <jgjsguilherme@gmail.com>
2021-10-17 12:45:39 -0300

    fix: refatorando codigo com base no typeorm

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthentucateUserController"
+import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthentucateUserController"
 
 const authenticateRoutes = Router();
 
