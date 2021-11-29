246d7447d014e8eeba2817b554eaa221bc20e073
Jamarin0 <jgjsguilherme@gmail.com>
2021-11-29 00:15:30 -0300

    feat: add migration Rentals

--- src/shared/infra/typeorm/migrations/1628366068220-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1628366068220-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1628366068220 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true 
                     },
                     {
                         name: "name",
