15ffc78955b78ffad7f314a3752020d7c52b2294
Jamarin0 <jgjsguilherme@gmail.com>
2021-12-19 12:33:37 -0300

    feat: listRentalsByUser

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -1,4 +1,4 @@
-import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDto";
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { getRepository, Repository } from "typeorm";
 import { Rental } from "../entities/Rental";
@@ -8,21 +8,36 @@ class RentalsRepository implements IRentalsRepository {
     constructor() {
         this.repository = getRepository(Rental);
     }
+  
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
-        const openByCar = await this.repository.findOne(car_id);
+        const openByCar = await this.repository.findOne({ where: { car_id, end_date: null}, });
         return openByCar;
     }
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
-        const openByUser = await this.repository.findOne(user_id);
+        const openByUser = await this.repository.findOne({ where: { user_id, end_date: null }, });
         return openByUser;
     }
-    async create({ car_id, expected_return_date, user_id }: ICreateRentalDTO): Promise<Rental> {
-        const rental = this.repository.create({ car_id, expected_return_date, user_id });
+    async create({ car_id, expected_return_date, user_id, id, end_date, total }: ICreateRentalDTO): Promise<Rental> {
+        const rental = this.repository.create({ car_id, expected_return_date, user_id, id, end_date, total });
 
         await this.repository.save(rental);
 
         return rental;
     }
+
+    async findById(id: string): Promise<Rental> {
+        const rental = await this.repository.findOne(id);
+        return rental;
+    }
+
+    async findByUser(user_id: string): Promise<Rental[]> {
+        const rentals = await this.repository.find({
+            where: {user_id},
+            relations: ["car"],
+        });
+        return rentals;
+    }
+
 }
 
 export { RentalsRepository }
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -4,17 +4,20 @@ import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in_memo
 import { AppError } from "@shared/errors/AppError";
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 let dayjsDateProvider: DayjsDateProvider;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 
 describe("Create Rental", () => {
     const dayAdd24hours = dayjs().add(1, "day").toDate();
     beforeEach(() => {
         rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
         dayjsDateProvider = new DayjsDateProvider();
-        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayjsDateProvider);
+        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayjsDateProvider, carsRepositoryInMemory);
     });
 
     it("should be able to create a new rental", async () => {
@@ -67,7 +70,7 @@ describe("Create Rental", () => {
                 car_id: "test",
                 expected_return_date: dayjs().toDate(),
             });
-         
+
         }).rejects.toBeInstanceOf(AppError);
     });
 })
