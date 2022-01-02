630df49ddf6345fcd5e7c01731e05d2804ab6a6a
Jamarin0 <jgjsguilherme@gmail.com>
2022-01-02 00:18:09 -0300

    fix: SendForgotMailSpec

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -69,11 +69,3 @@ describe("Create Category Controller", () => {
   });
 });
 
-
-// interface ICreateRentalDTO {
-//     user_id: string;
-//     car_id: string;
-//     expected_return_date: Date;
-// }
-
-// export { ICreateRentalDTO }
